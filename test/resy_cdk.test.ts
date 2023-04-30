1cdaaf66e20d3b3feb11fe538f6f15787be04db4
dchan331 <dchan331@gmail.com>
2023-04-29 22:03:24 -0400

    Initial commit

---
+++ test/resy_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ResyCdk from '../lib/resy_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/resy_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ResyCdk.ResyCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
